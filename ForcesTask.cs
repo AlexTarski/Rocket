c2a766ae9194225c8b236d6168b5b31a42353809
Alex Tarski <Alex.F.L.O.W@yandex.ru>
2024-05-28 16:27:48 +0300

    Add doodle control feature(use A and D keys for steering)

--- ForcesTask.cs
+++ ForcesTask.cs
@@ -1,4 +1,6 @@
-﻿namespace func_rocket;
+﻿using System;
+
+namespace func_rocket;
 
 public class ForcesTask
 {
@@ -6,15 +8,26 @@ public class ForcesTask
 	/// Создает делегат, возвращающий по ракете вектор силы тяги двигателей этой ракеты.
 	/// Сила тяги направлена вдоль ракеты и равна по модулю forceValue.
 	/// </summary>
-	public static RocketForce GetThrustForce(double forceValue) => r => Vector.Zero;
+	public static RocketForce GetThrustForce(double forceValue) => r => new Vector(Math.Cos(r.Direction) * forceValue, Math.Sin(r.Direction) * forceValue);
 
 	/// <summary>
 	/// Преобразует делегат силы гравитации, в делегат силы, действующей на ракету
 	/// </summary>
-	public static RocketForce ConvertGravityToForce(Gravity gravity, Vector spaceSize) => r => Vector.Zero;
+	public static RocketForce ConvertGravityToForce(Gravity gravity, Vector spaceSize) => r => gravity(spaceSize, r.Location);
 
 	/// <summary>
 	/// Суммирует все переданные силы, действующие на ракету, и возвращает суммарную силу.
 	/// </summary>
-	public static RocketForce Sum(params RocketForce[] forces) => forces[0];
+	public static RocketForce Sum(params RocketForce[] forces)
+    {
+			return r =>
+			{
+				var result = Vector.Zero;
+				foreach (var force in forces)
+				{
+                      result += force(r);
+                }
+                return result;
+			};
+    }
 }
