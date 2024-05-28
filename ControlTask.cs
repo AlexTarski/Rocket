6007f50ae816df32cbf474426c8bb2dc9d78a74d
Alex Tarski <Alex.F.L.O.W@yandex.ru>
2024-05-28 18:42:02 +0300

    Add autopilot feature, upd README

--- ControlTask.cs
+++ ControlTask.cs
@@ -1,9 +1,27 @@
+using System;
+
 namespace func_rocket;
 
 public class ControlTask
 {
-	public static Turn ControlRocket(Rocket rocket, Vector target)
-	{
-		return Turn.None;
-	}
+    private static double Angle;
+
+    public static Turn ControlRocket(Rocket rocket, Vector target)
+    {
+        var distanceVector = new Vector(target.X - rocket.Location.X, target.Y - rocket.Location.Y);
+
+        if (Math.Abs(distanceVector.Angle - rocket.Direction) < 0.5
+            || Math.Abs(distanceVector.Angle - rocket.Velocity.Angle) < 0.5)
+        {
+            Angle = (distanceVector.Angle - rocket.Direction + distanceVector.Angle - rocket.Velocity.Angle) / 2;
+        }
+        else
+        {
+            Angle = distanceVector.Angle - rocket.Direction;
+        }
+
+        if (Angle < 0)
+            return Turn.Left;
+        return Angle > 0 ? Turn.Right : Turn.None;
+    }
 }
--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # Rocket
+
+Game about "doodle" in the space. Use A and D keys to control doodle.
+
+Main goal – get into the hole.
+
+
+You can choose different levels to play:
+
+Zero – default gravity;
+
+Heavy – increased constant downward gravity;
+
+Up – upward gravity;
+
+WhiteHole – gravity push you from target;
+
+BlackHole – the pulling anomaly on the way to the target;
+
+BlackAndWhite – the pulling anomaly + pushing from target;
+
+ 
+
+By the default, the game uses the autopilot function for "doodle" to reach the target.
+
+To turn that function off, you need to return Turn.None in Control.Task class.
