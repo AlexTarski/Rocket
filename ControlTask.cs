848e8345c00af6cf0e1fce0f5f29b8bafc0647ca
Alex Tarski <Alex.F.L.O.W@yandex.ru>
2024-05-28 15:40:37 +0300

    Добавьте файлы проекта.

---
+++ ControlTask.cs
@@ -0,0 +1,9 @@
+namespace func_rocket;
+
+public class ControlTask
+{
+	public static Turn ControlRocket(Rocket rocket, Vector target)
+	{
+		return Turn.None;
+	}
+}
---
+++ ForcesTask.cs
@@ -0,0 +1,20 @@
+﻿namespace func_rocket;
+
+public class ForcesTask
+{
+	/// <summary>
+	/// Создает делегат, возвращающий по ракете вектор силы тяги двигателей этой ракеты.
+	/// Сила тяги направлена вдоль ракеты и равна по модулю forceValue.
+	/// </summary>
+	public static RocketForce GetThrustForce(double forceValue) => r => Vector.Zero;
+
+	/// <summary>
+	/// Преобразует делегат силы гравитации, в делегат силы, действующей на ракету
+	/// </summary>
+	public static RocketForce ConvertGravityToForce(Gravity gravity, Vector spaceSize) => r => Vector.Zero;
+
+	/// <summary>
+	/// Суммирует все переданные силы, действующие на ракету, и возвращает суммарную силу.
+	/// </summary>
+	public static RocketForce Sum(params RocketForce[] forces) => forces[0];
+}
